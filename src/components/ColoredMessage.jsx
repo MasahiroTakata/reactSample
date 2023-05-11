0a321fde2e44bbd4c346ad10c87f28b03c47aa71
Masahiro Takata <takata005581016@gmail.com>
2023-05-11 14:35:08 +0000

    propsの学習、一旦完了

--- src/components/ColoredMessage.jsx
+++ src/components/ColoredMessage.jsx
@@ -1,8 +1,10 @@
 export const ColoredMessage = (props) => {
+  const { color, children } = props; // colorとchildrenを「props.」を使わずに宣言できる
   const contentStyle = {
-    color: props.color,
+    color: color, // 「props.」の省略ができるが、今のようにプロパティ名と設定値が一緒の場合は・・・
+    color, // 値が同じ場合は、これも省略が可能
     fontSize: "20px",
   };
 
-  return <p style={contentStyle}>{props.children}</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
