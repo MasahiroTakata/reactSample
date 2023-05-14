b1fa1c60a850315403ea688e600acdb1c74cea79
Masahiro Takata <takata005581016@gmail.com>
2023-05-14 13:19:16 +0000

    Update 4 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { ColoredMessage } from "./components/ColoredMessage";
+import { CssModules } from "./components/CssModules";
 
-export const App = () => {
+const App = () => {
   // ボタンを押した時に実行する関数を定義
-  const onClickButton = () => {
-    alert("テスト");
-  };
+  // const onClickButton = () => {
+  //   alert("テスト");
+  // };
   // const contentPinkStyle = {
   //   color: "pink",
   //   fontSize: "20px",
@@ -14,12 +14,5 @@ export const App = () => {
   //   color: "pink",
   //   fontSize: "20px"
   // };
-  return (
-    <div>
-      <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
-      <ColoredMessage color="pink">元気です！</ColoredMessage>
-      <button onClick={onClickButton}>ボタン</button>
-    </div>
-  );
+  CssModules.render(<APP />, document.getElementById("root"));
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 import ReactDOM from "react-dom";
+import { CssModules } from "./components/CssModules";
 
-import { App } from "./App";
-ReactDOM.render(<App />, document.getElementById("root"));
-// import App from "./App";
-
-// const rootElement = document.getElementById("root");
-// const root = createRoot(rootElement);
-
-// root.render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
+ReactDOM.render(<CssModules />, document.getElementById("root"));
